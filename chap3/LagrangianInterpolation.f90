190c699be7d20fcf795358bc3b1208f1d251e5d8
chdlkl <2016126001@chd.edu.cn>
2018-03-01 23:26:03 +0800

    commit LagrangianInterpolation.f90

---
+++ chap3/LagrangianInterpolation.f90
@@ -0,0 +1,55 @@
+Program LagrangianInterpolation
+  Implicit none
+  Integer :: i, fileid 
+  Integer, parameter :: n1 = 51
+  Real(kind=8), parameter :: t0 = 0.d0, t1 = 1.d0
+  Real(kind=8) :: tmp, x(n1), y(n1)
+  
+  !// 获取已知点数据
+  Open ( newunit = fileid, file = 'origin.dat' )
+  Do i = 1, n1
+    x(i) = ( i-1 ) * ( t1-t0 ) / ( n1-1 )
+    y(i) = exp( x(i) )
+    Write ( fileid,'(2es)' ) x(i), y(i)
+  End do 
+  Close ( fileid )
+  !// 求插值
+  call GetP( x, y, n1, t0, t1 )
+End program LagrangianInterpolation
+  
+Subroutine GetP( x, y, n1, t0, t1 )
+  Implicit none
+  Integer :: i, j, k, fileid
+  Integer, intent(in) :: n1
+  Integer, parameter :: n = 101
+  Real(kind=8), intent(in) :: x(n1), y(n1), t0, t1 
+  Real(kind=8) :: xx(n), yy(n), L1(n1), L2(n1), tmp
+  
+  Open ( newunit = fileid, file = 'chazhi.dat' )
+  Do k = 1, n
+    xx(k) = ( k-1 ) * ( t1-t0 ) / ( n-1 )
+    Do i = 1, n1
+      L1(i) = 1.d0
+      Do j = 1, n1
+        If ( j /= i ) then
+          L1(i) = ( x(i) - x(j) ) * L1(i)  !// 求分母
+        End if 
+      End do
+      
+      L2(i) = 1.d0
+      Do j = 1, n1
+        If ( j /= i ) then
+          L2(i) = ( xx(k) - x(j) ) * L2(i)  !// 求分子
+        End if
+      End do
+    End do
+    
+    tmp = 0.d0
+    Do i = 1, n1
+      tmp = y(i) * L2(i) / L1(i) + tmp  !// 求插值项
+    End do
+    yy(k) = tmp
+    Write ( fileid,'(2es)' ) xx(k), yy(k)
+  End do
+  Close ( fileid )
+End subroutine GetP
