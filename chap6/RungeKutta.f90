273abcb1cd52477e5a3fc51c0874a629e7d17835
chdlkl <2016126001@chd.edu.cn>
2018-04-03 16:25:56 +0800

    update

--- chap6/RungeKutta.f90
+++ chap6/RungeKutta.f90
@@ -1,33 +1,38 @@
-!//---------------------------------------
-!//本程序以四阶Runge-Kutta算法
-!//u' = 4*t*sqrt(u), 0 =< t <= 2, u(0) = 1 精确解为：u(t) = (1+t^2)^2
-!//---------------------------------------
+!// y' = t*y + t^3
+!// y(t) = 3*exp(t^2/2) - t^2 - 2
 Module Runge_Kutta
   Implicit none
   Real(kind=8), parameter :: t0 = 0.d0, t1 = 1.d0
-  Real(kind=8), parameter :: h = 1d-2, num = nint((t1-t0)/h), u0=1.d0
+  Real(kind=8), parameter :: h = 1d-2, num = nint((t1-t0)/h), u0 = 1.d0
   Real(kind=8) :: k1, k2, k3, k4
-  Real(kind=8) :: t(0:Num),u(0:Num)
+  Real(kind=8) :: t(0:num), u(0:num), u_tmp
   Integer :: i
 Contains
 Subroutine solve()
   Implicit none
   u(0) = u0; t(0) = t0
   Do i = 1, num
-    k1 = 2.d0 * t(i-1) * u(i-1)
-    k2 = 2.d0 * ( t(i-1) + 0.5*h ) * ( u(i-1) + 0.5*h*k1 )
-    k3 = 2.d0 * ( t(i-1) + 0.5*h ) * ( u(i-1) + 0.5*h*k2 )
-    k4 = 2.d0 * ( t(i-1) + h ) * ( u(i-1) + h*k3 )
+    k1 = derfunc( t(i-1), u(i-1) )
+    k2 = derfunc( t(i-1)+h/2.d0, u(i-1)+h*k1/2.d0 )
+    k3 = derfunc( t(i-1)+h/2.d0, u(i-1)+h*k2/2.d0 )
+    k4 = derfunc( t(i-1)+h, u(i-1)+h*k3 )
     t(i) = t0 + dble(i)*h
     u(i) = u(i-1) + h / 6.d0 * ( k1 + 2.d0*k2 + 2.d0*k3 + k4 )
   End do
 
   open( 100, file = 'shujv.txt', status='unknown' )
   Do i = 0, num
-    write( 100,"(4f14.8)" ) t(i), exp( t(i)*t(i) ), u(i), abs( u(i)-exp(t(i)*t(i)) ) / exp( t(i)*t(i) ) * 100
+    u_tmp = 3.d0 * exp(t(i)**2/2.d0) - t(i)**2 - 2.d0
+    write( 100,"(4f14.8)" ) t(i), u_tmp, u(i), abs( u(i)-u_tmp ) / u_tmp * 100
   End do
   close(100)
 End subroutine solve
+
+Real(kind=8) function derfunc( t, u )
+  Implicit none
+  Real(kind=8), intent(in) :: t, u
+  derfunc = t * u + t**3
+End function derfunc
 End module Runge_kutta
   
 Program main
