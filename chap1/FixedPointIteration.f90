9a3177ca0d3d093466e34e74429c37117c14aa46
chdlkl <2016126001@chd.edu.cn>
2018-04-15 15:15:01 +0800

    update

--- chap1/FixedPointIteration.f90
+++ chap1/FixedPointIteration.f90
@@ -1,4 +1,5 @@
-Program FixedPointIteration  !// 使用不动点迭代方法求解方程 x^3 + x - 1 = 0 的零点
+Program FixedPointIteration  
+  !// 使用不动点迭代方法求解方程 x^3 + x - 1 = 0 的零点
   Implicit none 
   Real(kind=8), parameter :: eps = 1.d-8
   Real(kind=8), external :: func
@@ -21,4 +22,6 @@ End function func
 !// 方程 x^3 + x - 1 = 0 主要的不动点格式有三种
 !// 第一种：x = 1 - x^3 这一种格式不收敛
 !// 第二种：x = ( 1 - x^3 )^(1/3)，这种格式收敛
-!// 第三种：就是此代码中的收敛格式。x = ( 1 + 2*x^3 ) / ( 1 + 3*x^2 )，这种格式不仅收敛，而且收敛速度快，精度高
+!// 第三种：就是此代码中的收敛格式。x = ( 1 + 2*x^3 ) / ( 1 + 3*x^2 )
+!// 这种格式不仅收敛，而且收敛速度快，精度高
+
